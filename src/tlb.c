53c52cec6e1b0d5dd0d35f3d8db76f8c5c4af815
Marta <martavianamachado@gmail.com>
2025-10-03 18:06:21 +0100

    translate

--- src/tlb.c
+++ src/tlb.c
@@ -57,5 +57,33 @@ void tlb_invalidate(va_t virtual_page_number) {
 
 pa_dram_t tlb_translate(va_t virtual_address, op_t op) {
   // TODO: implement the TLB logic.
+  for (int i = 0; i < TLB_L1_SIZE; i++) {
+    if (tlb_l1[i].virtual_page_number == (virtual_address >> PAGE_OFFSET_BITS)) {
+      tlb_l1_hits++;
+      tlb_l1[i].last_access = get_time();
+      if (op == OP_WRITE)
+        tlb_l1[i].dirty = true;
+      return tlb_l1[i].physical_page_number;
+    }
+  }
+
+  tlb_l1_misses++;
+  int lru_index = 0;
+  uint64_t min_access = tlb_l1[0].last_access;
+
+  for (int i = 1; i < TLB_L1_SIZE; i++) {
+      if (tlb_l1[i].last_access < min_access) {
+          min_access = tlb_l1[i].last_access;
+          lru_index = i;
+      }
+  }
+
+  tlb_l1[lru_index].virtual_page_number = virtual_address >> PAGE_OFFSET_BITS;
+  tlb_l1[lru_index].physical_page_number = page_table_translate(virtual_address, op);
+  tlb_l1[lru_index].last_access = get_time();
+  tlb_l1[lru_index].valid = true;
+  tlb_l1[lru_index].dirty = (op == OP_WRITE);
+
   return page_table_translate(virtual_address, op);
 }
+
