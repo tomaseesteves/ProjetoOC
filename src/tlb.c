036b43640014b01b9baaa8f8c4c18d83d8bea955
danielaalves01 <56894701+danielaalves01@users.noreply.github.com>
2025-10-04 15:38:01 +0100

    FIX: tlb_translate and tlb_invalidate
    tlb_invalidate:
    -> acrescentei a função increment_time
    
    tlb_translate:
    -> acresecentei fase de Write-Back
    -> if checks mais fortes (verificam valid e dirty bits)
    -> mensagem de eviction imprime em hexadecimais (antes imprimia em decimais)

--- src/tlb.c
+++ src/tlb.c
@@ -55,26 +55,29 @@ void tlb_invalidate(va_t virtual_page_number)
   // TODO: implement TLB entry invalidation.
   for (int i = 0; i < TLB_L1_SIZE; i++)
   {
-    if (tlb_l1[i].virtual_page_number == virtual_page_number)
+    if (tlb_l1[i].valid && tlb_l1[i].virtual_page_number == virtual_page_number)
     {
       tlb_l1[i].valid = false;
       tlb_l1_invalidations++;
+      increment_time(TLB_L1_LATENCY_NS);
       break;
     }
   }
+  increment_time(TLB_L1_LATENCY_NS);
 }
 
 pa_dram_t tlb_translate(va_t virtual_address, op_t op)
 {
   // TODO: implement the TLB logic.
+  va_t virtual_page_offset = virtual_address & PAGE_OFFSET_MASK;
+
   for (int i = 0; i < TLB_L1_SIZE; i++)
   {
-    // If page if found:
-    if (tlb_l1[i].virtual_page_number == ((virtual_address >> PAGE_SIZE_BITS) & PAGE_INDEX_MASK))
+    // If page is found:
+    if ((tlb_l1[i].virtual_page_number == ((virtual_address >> PAGE_SIZE_BITS) & PAGE_INDEX_MASK)) && tlb_l1[i].valid)
     {
       tlb_l1_hits++;
       tlb_l1[i].last_access = get_time();
-      va_t virtual_page_offset = virtual_address & PAGE_OFFSET_MASK;
       if (op == OP_WRITE)
         tlb_l1[i].dirty = true;
 
@@ -118,11 +121,18 @@ pa_dram_t tlb_translate(va_t virtual_address, op_t op)
   increment_time(TLB_L1_LATENCY_NS);
   pa_dram_t translated_access = page_table_translate(virtual_address, op);
 
-  log_dbg("Evicting TLB L1 entry i=%d VPN=%" PRIu64 " PPN=%" PRIu64 " valid=%d dirty=%d...",
+  log_dbg("Evicting TLB L1 entry i=%d VPN=%" PRIx64 " PPN=%" PRIx64 " valid=%d dirty=%d...",
       new_index, tlb_l1[new_index].virtual_page_number, 
       tlb_l1[new_index].physical_page_number, 
       tlb_l1[new_index].valid, tlb_l1[new_index].dirty
   );
+
+  // Write Back Policy
+  if (tlb_l1[new_index].valid && tlb_l1[new_index].dirty) {
+    log_dbg("***** TLB L1 write back *****");
+    pa_dram_t evicted_address = (tlb_l1[new_index].physical_page_number << PAGE_SIZE_BITS);
+    write_back_tlb_entry(evicted_address);
+  }
 
   tlb_l1[new_index].virtual_page_number = (virtual_address >> PAGE_SIZE_BITS) & PAGE_INDEX_MASK;
   tlb_l1[new_index].physical_page_number = (translated_access >> PAGE_SIZE_BITS) & PAGE_INDEX_MASK;
